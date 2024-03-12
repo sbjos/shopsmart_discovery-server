c493be0e5a2dbde0b00fa8bb5249933c289488c2
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-11 21:07:10 -0400

    Cleanup

--- src/main/java/com/shopsmart/discoveryserver/config/SecurityConfig.java
+++ src/main/java/com/shopsmart/discoveryserver/config/SecurityConfig.java
@@ -1,19 +1,16 @@
 package com.shopsmart.discoveryserver.config;
 
-import jakarta.activation.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
     @Value("${eureka.username}")
@@ -24,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsService(AuthenticationManagerBuilder auth) throws Exception {
         return new InMemoryUserDetailsManager(
-                User.withDefaultPasswordEncoder()
+                User.withDefaultPasswordEncoder() // No PasswordEncoder for testing purposes
                         .username(username)
                         .password(password)
                         .roles("USER")
